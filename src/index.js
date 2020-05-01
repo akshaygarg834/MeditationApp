c79c815897aa3fdfa44fd321964872a2ad9876ff
akshaygarg834 <akshaygarg834@gmail.com>
2020-05-02 02:10:06 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import "./styles.css";
+console.time("AppStart");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+console.log("asdfghjf");
+
+console.timeEnd("AppStart");
